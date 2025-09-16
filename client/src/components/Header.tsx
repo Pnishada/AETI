45bf082ba548d077e7a2fec7a8fee57c00104651
sithnuwanpramudith <sithnuwanpramudith2000@gmail.com>
2025-09-16 13:26:55 +0530

    Add staff page

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -14,11 +14,12 @@ export default function Header() {
   const navLinks = [
     { name: "Home", route: "/" },
     { name: "Discover AETI", route: "/about" },
-    { name: "Courses", sectionId: "/courses" },
+    { name: "Courses", sectionId: "/CoursesSection" },
     { name: "Downloads", route: "/download" },
     { name: "Gallery", route: "/gallery" },
     { name: "LMS", route: "/lms" },
     { name: "Contact Us", route: "/Contact" },
+    { name: "Staff", route: "/staff" },
   ];
 
   const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
