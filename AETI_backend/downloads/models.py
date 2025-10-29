2363dccf6104f41b2e3ff7d6b9532ffd9ec3a828
Dinusha Fernando <dinushafdo9998@gmail.com>
2025-10-29 12:23:09 +0530

    add requirement.txt file

--- AETI_backend/downloads/models.py
+++ AETI_backend/downloads/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Download(models.Model):
     title = models.CharField(max_length=200)
-    file = models.FileField(upload_to='downloads/', max_length=500)  # Increased limit
+    file = models.FileField(upload_to='downloads/', max_length=500)  
     description = models.TextField(blank=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
